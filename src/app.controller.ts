a30884464a7a718aafa7dd5f1158c25dea000c37
hajikenji <92619325+hajikenji@users.noreply.github.com>
2024-03-12 12:47:23 +0000

    add リダイレクト処理を追加

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Controller, Get, Post, Redirect } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -18,5 +18,10 @@ export class AppController {
   @Post('new')
   responseNew(): string {
     return this.appService.responseNew();
+  }
+
+  @Get('redirect')
+  @Redirect('/index')
+  redirect(): void {
   }
 }
